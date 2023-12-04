37b4b8df09e69a35e8c38a9eb7cc55b14e2836d4
kim yun_hae <kyhtq2@gmail.com>
2023-12-04 16:28:08 +0900

    슬라이드형식 추가 및 속도 조절 ver.05

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 window.onload = function(){
-  
+  //SWIPER
+  const swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    on: {
+      //슬라이드 변경시 텍스트 페이드인
+      slideChangeTransitionStart: function() {
+        let title = $('.swiper-slide-active .building_txt .title')
+        let txt_p = $('.swiper-slide-active .building_txt>p')
+        let view = $('.swiper-slide-active .building_txt div p')
+        title.animate({opacity: 1},1000)
+        txt_p.delay(500).animate({opacity: 1},1000)
+        view.delay(1000).animate({opacity: 1},1000)
+      }
+    }
+  });
+
   $(function(){
     // FULL PAGE
     $('.full_page').mousewheel(function(event, delta){
@@ -21,64 +39,52 @@ window.onload = function(){
     e.preventDefault()
     $(window).scrollTo({top:0,left:0},2000)
   })
+  $('.header p').click(function(e){
+    e.preventDefault()
+    $(window).scrollTo({top:0,left:0},2000)
+  })
+  
 
   $(window).scroll(function(){
-    // 1-4페이지 글자 페이드인
-    if($(this).scrollTop() >= $('#page01').offset().top -3) {
-      $('#page01 .title').fadeIn(1300)
-      $('#page01 p').animate({opacity: 1},2000)
-      $('#page01 .bt_view').delay(400).animate({opacity: 1},1300)
-    } else {
-      // $('#page01').children().fadeOut(500)
+    // // swiper 첫페이지 글자 페이드인
+    if($(this).scrollTop() >= $('#building').offset().top) {
+      $('.swiper01 .building_txt h2').animate({opacity: 1},1000)
+      $('.swiper01 .building_txt>p').delay(500).animate({opacity: 1},1000)
+      $('.swiper01 .building_txt div p').delay(1000).animate({opacity: 1},1000)
     }
-    if($(this).scrollTop() >= $('#page02').offset().top -3) {
-      $('#page02 .title').fadeIn(1300)
-      $('#page02 p').animate({opacity: 1},2000)
-      $('#page02 .bt_view').delay(400).animate({opacity: 1},1300)
-    } 
-    if($(this).scrollTop() >= $('#page03').offset().top -3) {
-      $('#page03 .title').fadeIn(1300)
-      $('#page03 p').animate({opacity: 1},2000)
-      $('#page03 .bt_view').delay(400).animate({opacity: 1},1300)
-      
-    }
-    if($(this).scrollTop() >= $('#page04').offset().top -3) {
-      $('#page04 .title').fadeIn(1300)
-      $('#page04 p').animate({opacity: 1},2000)
-      $('#page04 .bt_view').delay(400).animate({opacity: 1},1300)
-    } 
+
 
     //5-7페이지 글자 슬라이드인
-    if($(this).scrollTop() >= $('#page05').offset().top -2) {
-      $('#page05 p:first-child').animate({ left: '0'},3000)
-      $('#page05 p:nth-child(2)').animate({ right: '0'},3000).animate({ right: '0'},1000).animate({ right: '100vw'},1500).animate({ right: '100vw'},3000)
-      $('#page05 p:last-child').animate({ left: '0'},3000).animate({ left: '0'},1000).animate({ left: '100vw'},1500).animate({ left: '100vw'},3000)
-      $('#page05 .page_intro').delay(5000).fadeOut(1000)
-      $('#page05 .page_infor').delay(6000).fadeIn(500)
-      $('#page05 .page_infor h2').delay(5500).fadeIn(1000)
-      $('#page05 .page_infor h4').delay(6000).fadeIn(1000)
-      
+    if($(this).scrollTop() >= $('#page05').offset().top -0) {
+      $('#page05 p').fadeIn(1500)
+      $('#page05 p:nth-child(2)').animate({ right: '100vw'},1500)
+      $('#page05 p:last-child').animate({ left: '100vw'},1500)
+      $('#page05 .page_intro').delay(2500).fadeOut(500)
+      $('#page05 .page_infor').delay(3000).fadeIn(1000)
+      $('#page05 .page_infor h2').delay(3500).fadeIn(1000)
+      $('#page05 .page_infor h4').delay(4000).fadeIn(1000)
+
     }
-    if($(this).scrollTop() >= $('#page06').offset().top -2) {
-      $('#page06 p:first-child').animate({ left: '0'},3000).animate({ left: '0'},1000).animate({ left: '-100vw'},1500).animate({ left: '-100vw'},3000)
-      $('#page06 p:nth-child(2)').animate({ right: '0'},3000)
-      $('#page06 p:last-child').animate({ left: '0'},3000).animate({ left: '0'},1000).animate({ left: '100vw'},1500).animate({ left: '100vw'},3000)
-      $('#page06 .page_intro').delay(5000).fadeOut(1000)
-      $('#page06 .page_infor').delay(6000).fadeIn(500)
-      $('#page06 .page_infor h2').delay(5500).fadeIn(1000)
-      $('#page06 .page_infor h4').delay(6500).fadeIn(1000)
+    if($(this).scrollTop() >= $('#page06').offset().top -300) {
+      $('#page06 p').fadeIn(1500)
+      $('#page06 p:first-child').animate({ left: '100vw'},1500)
+      $('#page06 p:last-child').animate({ right: '100vw'},1500)
+      $('#page06 .page_intro').delay(2500).fadeOut(500)
+      $('#page06 .page_infor').delay(3000).fadeIn(1000)
+      $('#page06 .page_infor h2').delay(3500).fadeIn(1000)
+      $('#page06 .page_infor h4').delay(4000).fadeIn(1000)
     }
-    if($(this).scrollTop() >= $('#page07').offset().top -2) {
-      $('#page07 p:first-child').animate({ left: '0'},3000).animate({ left: '0'},1000).animate({ left: '-100vw'},1500).animate({ left: '-100vw'},3000)
-      $('#page07 p:nth-child(2)').animate({ right: '0'},3000).animate({ left: '0'},1000).animate({ left: '100vw'},1500).animate({ left: '100vw'},3000)
-      $('#page07 p:last-child').animate({ left: '0'},3000)
-      $('#page07 .page_intro').delay(5000).fadeOut(1000)
-      $('#page07 .page_infor').delay(6000).fadeIn(500)
-      $('#page07 .page_infor h2').delay(5500).animate({opacity: 1},1000)
-      $('#page07 .page_infor h4').delay(6500).animate({opacity: 1},1000)
+    if($(this).scrollTop() >= $('#page07').offset().top -300) {
+      $('#page07 p').fadeIn(1500)
+      $('#page07 p:first-child').animate({ left: '100vw'},1500)
+      $('#page07 p:nth-child(2)').animate({ right: '100vw'},1500)
+      $('#page07 .page_intro').delay(2500).fadeOut(500)
+      $('#page07 .page_infor').delay(3000).fadeIn(1000)
+      $('#page07 .page_infor h2').delay(3500).animate({opacity: 1},1000)
+      $('#page07 .page_infor h4').delay(4000).animate({opacity: 1},1000)
     }
 
-    if($(this).scrollTop() >= $('#page08').offset().top -2) {
+    if($(this).scrollTop() >= $('#page08').offset().top -5) {
       $('#page08 .meier img').fadeIn(1500)
       $('#page08 .title').animate({opacity: 1},1300)
       $('#page08 .profile').delay(1000).animate({opacity: 1},1300)
@@ -89,7 +95,22 @@ window.onload = function(){
   })
   if(matchMedia("screen and (min-width: 769px").matches) {
     $('#page08 .meier img').attr('src','./images/MEIER1.jpg')
+    $('.building_img2').mouseenter(function(){
+      $('.building_img2 img').animate({opacity: 0.5},200)
+      $('.bt_view').animate({opecity: 1},200)
+      $('.building_img2').mouseleave(function(){
+        $('.bt_view').animate({opecity: 0},200)
+        $('.building_img2 img').delay(300).animate({opacity: 1},200)
+      })
+    })
+    $('.bt_view').mouseenter(function(){
+      $('.building_img2 img').animate({opacity: 0.5})
+      $('.bt_view').mouseleave(function(){
+        $('.bt_view').animate({opecity: 0})
+      })
+    })
   }
+
 // COLORBOX
   // 주빌리교회 컬러박스
   $(".view_popup1").colorbox({
